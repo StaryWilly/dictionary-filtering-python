bc5ad002e66ef8c4097ac3ad8ea9aab0904cf6fe
StaryWilly <starywilly@gmail.com>
2020-07-23 16:10:37 +0200

    First commmit. Done: - password generators in bash and python generate.sh generator.py - analiza slownikowa filtrowanie po dlugosci znakow kasowanie duplikatow - zaczete: split i join remote ssh

--- remote_ssh.py
+++ remote_ssh.py
@@ -1,21 +1,48 @@
 #remote_ssh.py
 from paramiko.client import SSHClient
+import paramiko
 #pip3 install paramiko
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 source_file = open("ip_list.cfg", "r")
-print("Source:")
-print(source_file.read())
+#print("Source:")
+#print(source_file.read())
 
 with open('ip_list.cfg', 'r') as f:
-    l = [[num for num in line.split(' ')] for line in f]
-print(l)
-print(l[0])
-print(l[1])
-print(l[2])
+    l = [[num for num in line.split(' ')] for line in f if line.strip() != ""]
+
+#print(l)
+print(l[0][0])
+print(l[0][1])
+print(l[0][2])
 
 
-client = SSHClient()
-client.load_system_host_keys()
-client.connect('linuxip', username='your_user', password='very_secret')
-stdin, stdout, stderr = client.exec_command('python /home/your_user/your/path/to/scripty.py')
+def execute_command_readlines(address, usr, pwd, command):
+    try:
+        logger.debug("ssh " + usr + "@" + address + ", running : " +
+                     command)
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        client.connect(address, username=usr, password=pwd)
+        _, ss_stdout, ss_stderr = client.exec_command(command)
+        r_out, r_err = ss_stdout.readlines(), ss_stderr.read()
+        logger.debug(r_err)
+        if len(r_err) > 5:
+            r_err[-1] = r_err[-1].strip()
+            logger.error(r_err)
+        else:
+            r_out[-1] = r_out[-1].strip()
+            logger.debug(r_out)
+        client.close()
+    except IOError:
+        logger.warning(".. host " + address + " is not up")
+        return "host not up", "host not up"
+
+    return r_out, r_err
+result = execute_command_readlines("netbook", "willy", "sirozzy", "pwd")
+print(result[0])
