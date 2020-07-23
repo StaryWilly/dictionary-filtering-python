1a8a9ab74dff7de765ff93db4348e96969dd7a6f
StaryWilly <starywilly@gmail.com>
2020-07-23 13:51:32 +0200

    First commmit. Done: - password generators in bash and python generate.sh generator.py - analiza slownikowa filtrowanie po dlugosci znakow kasowanie duplikatow - zaczete: split i join remote ssh

--- remote_ssh.py
+++ remote_ssh.py
@@ -1,7 +1,17 @@
 #remote_ssh.py
 from paramiko.client import SSHClient
+#pip3 install paramiko
 
-client = SSHClient()
-client.load_system_host_keys()
-client.connect('linuxip', username='your_user', password='very_secret')
-stdin, stdout, stderr = client.exec_command('python /home/your_user/your/path/to/scripty.py')
+
+source_file = open("ip_list.cfg", "r")
+print("Source:")
+print(source_file.read())
+
+with open('ip_list.cfg', 'r') as f:
+    l = [[num for num in line.split(' ')] for line in f]
+print(l)
+
+# client = SSHClient()
+# client.load_system_host_keys()
+# client.connect('linuxip', username='your_user', password='very_secret')
+# stdin, stdout, stderr = client.exec_command('python /home/your_user/your/path/to/scripty.py')
